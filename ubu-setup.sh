d1a0f779008fa5f4a0dbc4b8a8d1c17424307aa2
Sulaiman3352 <32817650+Sulaiman3352@users.noreply.github.com>
2024-04-28 12:08:23 +0300

    Add files via upload

--- ubu-setup.sh
+++ ubu-setup.sh
@@ -1,45 +1,47 @@
 #!/bin/sh
-echo " Disable snapd services "
-sudo systemctl disable snapd.service
-sudo systemctl disable snapd.socket
-sudo systemctl disable snapd.seeded.service
-sudo systemctl mask snapd.service
+
 snap list
-echo "what is the version of core?"
-read core
 
 echo "start removing packages"
 
-sudo snap remove $core
-sudo snap remove firefox
-sudo snap remove snap-store
-sudo snap remove gtk-common-themes
-sudo snap remove snapd-desktop-integration
+sudo snap remove --purge firefox
+sudo snap remove --purge snap-store
+sudo snap remove --purge gtk-common-themes
+sudo snap remove --purge snapd-desktop-integration
 echo "what is the version of gnome?"
 read gnome
-sudo snap remove $gnome
+sudo snap remove --purge gnome*
+sudo snap remove --purge bare
+echo "what is the version of core?"
+read core
+sudo snap remove --purge core*
 
-echo "deleting cache snapd... "
-sudo rm -rf /var/cache/snapd/
+echo "removing snapd... "
+sudo snap remove --purge snapd
+sudo apt remove --autoremove snapd
 
-echo "purge snapd... "
-sudo apt autoremove --purge snapd
-
-rm -rf ~/snap
+echo "adding firefox repo"
+sudo add-apt-repository ppa:mozillateam/ppa
 
 echo "block apt to download firefox from snapd... "
+touch /etc/apt/preferences.d/nosnap.pref && echo "Package: snapd
+Pin: release a=*
+Pin-Priority: -10" > /etc/apt/preferences.d/nosnap.pref
 
-touch /etc/apt/preferences.d/firefox-no-snap && echo "Package: firefox*
-Pin: release o=Ubuntu*
-Pin-Priority: -1" > /etc/apt/preferences.d/firefox-no-snap
-
-echo "adding repo and install firfox"
-sudo add-apt-repository ppa:mozillateam/ppa
 sudo apt update
-sudo apt install firefox
+
+sudo apt install -t 'o=LP-PPA-mozillateam' firefox
+sudo apt install --install-suggests gnome-software
 
 echo "installing flatpak"
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo apt install gnome-software-plugin-flatpak gnome-software
 
+echo "Final touch"
+
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+
+touch /etc/apt/preferences.d/mozillateamppa && echo "Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 501" > /etc/apt/preferences.d/mozillateamppa
